e5230b3e487c4643d9bce73862f6e4a89339242f
VeraE-design <kevwese@gmail.com>
2025-05-13 15:44:42 +0100

    updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,7 @@
 const USER = require("../models/user");
 const bcrypt = require("bcryptjs");
+const generateToken = require("../helpers/generateToken");
+const { sendWelcomeEmail } = require("../email/sendEmail");
 
 const handleRegister = async (req, res) => {
   const { fullName, email, password, phoneNumber, role } = req.body;
@@ -17,6 +19,8 @@ const handleRegister = async (req, res) => {
     const salt = await bcrypt.genSalt();
     const hashedPassword = await bcrypt.hash(password, salt);
     // verify process
+    const verificationToken = generateToken();
+    const verificationTokenExpires = Date.now() + 24 * 60 * 60 * 1000;
     // save to db
     const user = await USER.create({
       fullName,
@@ -24,7 +28,17 @@ const handleRegister = async (req, res) => {
       password: hashedPassword,
       role: role || "tenant",
       phoneNumber,
+      verificationToken,
+      verificationTokenExpires,
     });
+    // send an email
+    const clientUrl = `${process.env.FRONTEND_URL}/verify-email/${verificationToken}`;
+    await sendWelcomeEmail({
+      email: user.email,
+      fullName: user.fullName,
+      clientUrl,
+    });
+
     return res
       .status(201)
       .json({ success: true, message: "User Registered successfully", user });
@@ -34,4 +48,31 @@ const handleRegister = async (req, res) => {
   }
 };
 
-module.exports = { handleRegister };
+const handleVerifyEmail = async (req, res) => {
+  const { token } = req.params;
+  try {
+    const user = await USER.findOne({
+      verificationToken: token,
+      verificationTokenExpires: { $gt: Date.now() },
+    });
+    if (!user) {
+      return res
+        .status(404)
+        .json({ message: "Invalid or expired token",  });
+    }
+    // mark the user as verified
+    user.isVerified = true;
+    user.verificationToken = undefined;
+    user.verificationTokenExpires = undefined;
+    await user.save();
+    return res.status.json({
+      success: true,
+      message: "Email Verified Successfully",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { handleRegister, handleVerifyEmail };
