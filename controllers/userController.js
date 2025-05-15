863442e2f5ee81c589f992180f943f845d28eb54
VeraE-design <kevwese@gmail.com>
2025-05-15 12:52:58 +0100

    feat: added login logic

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,7 @@ const USER = require("../models/user");
 const bcrypt = require("bcryptjs");
 const generateToken = require("../helpers/generateToken");
 const { sendWelcomeEmail } = require("../email/sendEmail");
+const jwt = require("jsonwebtoken");
 
 const handleRegister = async (req, res) => {
   const { fullName, email, password, phoneNumber, role } = req.body;
@@ -83,5 +84,56 @@ const handleVerifyEmail = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+// handle login
 
-module.exports = { handleRegister, handleVerifyEmail };
+const handleLogin = async (req, res) => {
+  const { email, password, role } = req.body;
+  if (!email || !password || !role) {
+    return res
+      .status(400)
+      .json({ message: "Email, password, and role are required" });
+  }
+  try {
+    const user = await USER.findOne({ email });
+    if (!user) {
+      return res
+        .status(401)
+        .json({ message: "Account Not found, please register" });
+    }
+    if (user.role !== role) {
+      return res.status(403).json({ message: "Access Denied for this role" });
+    }
+    if (!user.isVerified) {
+      return res
+        .status(403)
+        .json({ message: "Email Not verified, Check your mail" });
+    }
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordCorrect) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+    // generate a token
+    const token = jwt.sign(
+      { email: user.email, role: role.user },
+      process.env.JWT_SECRET,
+      { expiresIn: "3 days" }
+    );
+
+    return res.status(200).json({
+      success: true,
+      token,
+      user: {
+        fullName: user.fullName,
+        email: user.email,
+        profilePicture: user.profilePicture,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { handleRegister, handleVerifyEmail, handleLogin };
